de8ddd84a3b20dce4f7ddcfdd866eb141dde235a
anshay <asoni2@ncsu.edu>
2016-09-04 17:49:57 -0400

    added loop over head

--- measurement_overhead.c
+++ measurement_overhead.c
@@ -4,8 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <assert.h>
 #include <sched.h>
+#include <preempt.h>
 #include <sys/syscall.h>
 #include <time.h>
 
@@ -16,6 +16,8 @@ int main(int agrc, char *argv[])
 	unsigned cycles_high1; 
 	unsigned cycles_low1; 
 	uint64_t start,end;
+
+	//preempt_disable();
 	asm volatile ("cpuid\n\t"
 		  "rdtsc\n\t"
 		  "mov %%edx, %0\n\t"
@@ -32,13 +34,80 @@ int main(int agrc, char *argv[])
 		  : "=r" (cycles_high1), "=r" (cycles_low1)
 		  :: "%rax", "%rbx", "%rcx", "%rdx");
 
+	//preempt_enable();
 	start = ( ((uint64_t)cycles_high0 << 32) | cycles_low0 ); 
    end = ( ((uint64_t)cycles_high1 << 32) | cycles_low1 ); 
 
 	printf("high0 = %d, high1 = %d, low0 = %d, low1 = %d\n",cycles_high0,cycles_high1,cycles_low0,cycles_low1);
 	printf("measurement overhead time = %llu clock cycles\n",(end-start));
+
+
+	// loop overhead measurement
 	
-		
+	volatile int a;
+	uint64_t cycles_without_loop, cycles_with_loop;
+
+	a=0;
+	asm volatile ("cpuid\n\t"
+		  "rdtsc\n\t"
+		  "mov %%edx, %0\n\t"
+		  "mov %%eax, %1\n\t"
+		  : "=r" (cycles_high0), "=r" (cycles_low0)
+		  :: "%rax", "%rbx", "%rcx", "%rdx");
+
+	a++;
+	a++;
+	a++;
+	a++;
+	a++;
+	a++;
+	a++;
+	a++;
+	a++;
+	a++;
+
+	asm volatile ("rdtscp\n\t"
+		  "mov %%edx, %0\n\t"
+		  "mov %%eax, %1\n\t"
+		  "cpuid\n\t"
+		  : "=r" (cycles_high1), "=r" (cycles_low1)
+		  :: "%rax", "%rbx", "%rcx", "%rdx");
+
+	start = ( ((uint64_t)cycles_high0 << 32) | cycles_low0 ); 
+   end = ( ((uint64_t)cycles_high1 << 32) | cycles_low1 ); 
+
+	cycles_without_loop = end-start;
+
+	printf("high0 = %d, high1 = %d, low0 = %d, low1 = %d\n",cycles_high0,cycles_high1,cycles_low0,cycles_low1);
+	printf("cycles taken without loop = %llu clock cycles\n",cycles_without_loop);
+	
+	a=0;
+	
+	asm volatile ("cpuid\n\t"
+		  "rdtsc\n\t"
+		  "mov %%edx, %0\n\t"
+		  "mov %%eax, %1\n\t"
+		  : "=r" (cycles_high0), "=r" (cycles_low0)
+		  :: "%rax", "%rbx", "%rcx", "%rdx");
+
+	while(a<10)
+		a++;
+	
+	asm volatile ("rdtscp\n\t"
+	  "mov %%edx, %0\n\t"
+	  "mov %%eax, %1\n\t"
+	  "cpuid\n\t"
+	  : "=r" (cycles_high1), "=r" (cycles_low1)
+	  :: "%rax", "%rbx", "%rcx", "%rdx");
+
+	start = ( ((uint64_t)cycles_high0 << 32) | cycles_low0 ); 
+	end = ( ((uint64_t)cycles_high1 << 32) | cycles_low1 ); 
+	
+	cycles_with_loop = end-start;
+	
+	printf("high0 = %d, high1 = %d, low0 = %d, low1 = %d\n",cycles_high0,cycles_high1,cycles_low0,cycles_low1);
+	printf("cycles taken without loop = %llu clock cycles\n",cycles_with_loop);
+
 
 }
 
