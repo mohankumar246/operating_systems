1c04ecae31e24ca6d075eb87cc5a21a88e8b2831
anshay <asoni2@ncsu.edu>
2016-09-04 15:33:48 -0400

    updated measurement overhead file

--- measurement_overhead.c
+++ measurement_overhead.c
@@ -1,5 +1,5 @@
-#ifndef MEASUREMENT_OVERHEAD_CPP
-#define MEASUREMENT_OVERHEAD_CPP
+#ifndef MEASUREMENT_OVERHEAD_C
+#define MEASUREMENT_OVERHEAD_C
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,6 +15,7 @@ int main(int agrc, char *argv[])
 	unsigned cycles_low0; 
 	unsigned cycles_high1; 
 	unsigned cycles_low1; 
+	uint64_t start,end;
 	asm volatile ("cpuid\n\t"
 		  "rdtsc\n\t"
 		  "mov %%edx, %0\n\t"
@@ -30,6 +31,12 @@ int main(int agrc, char *argv[])
 		  "cpuid\n\t"
 		  : "=r" (cycles_high1), "=r" (cycles_low1)
 		  :: "%rax", "%rbx", "%rcx", "%rdx");
+
+	start = ( ((uint64_t)cycles_high0 << 32) | cycles_low0 ); 
+   end = ( ((uint64_t)cycles_high1 << 32) | cycles_low1 ); 
+
+	printf("high0 = %d, high1 = %d, low0 = %d, low1 = %d\n",cycles_high0,cycles_high1,cycles_low0,cycles_low1);
+	printf("measurement overhead time = %llu clock cycles\n",(end-start));
 	
 		
 
---
+++ measurement_overhead.tcl
@@ -0,0 +1,2 @@
+rm -f measure
+gcc -g -O3 measurement_overhead.c -o measure
