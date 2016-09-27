eeb1aa9b062a3ebca4f00d3c6e09ab82f6e0acb5
anshay <asoni2@ncsu.edu>
2016-09-27 19:09:36 -0400

    chages to page fault

--- src/page_fault.c
+++ src/page_fault.c
@@ -25,7 +25,7 @@ int main(int agrc, char *argv[])
 	off_t offset;
 	struct stat sb;
 	size_t len;
-	int fd;
+	int fd,unm;
 	char *op,test;
 
 	uint64_t i;
@@ -88,7 +88,7 @@ int main(int agrc, char *argv[])
 							: "=r" (cycles_high0), "=r" (cycles_low0)
 							:: "%rax", "%rbx", "%rcx", "%rdx");
 
-		test= *op;
+		test= op[i];
 
 		
 		asm volatile ("rdtscp\n\t"
@@ -105,31 +105,13 @@ int main(int agrc, char *argv[])
 			printf("FOUND %d %llu\n",i,(end-start));
 
 		// accessing next byte - should be a page hit
-		op++;
+		//op++;
 	}
-	//asm volatile ("cpuid\n\t"
-	//					"rdtsc\n\t"
-	//					"mov %%edx, %0\n\t"
-	//					"mov %%eax, %1\n\t"
-	//					: "=r" (cycles_high0), "=r" (cycles_low0)
-	//					:: "%rax", "%rbx", "%rcx", "%rdx");
-
-	//test= *op;
-
-	//
-	//asm volatile ("rdtscp\n\t"
-	//					"mov %%edx, %0\n\t"
-	//					"mov %%eax, %1\n\t"
-	//					"cpuid\n\t"
-	//					: "=r" (cycles_high1), "=r" (cycles_low1)
-	//					:: "%rax", "%rbx", "%rcx", "%rdx");
-	//start = ( ((uint64_t)cycles_high0 << 32) | cycles_low0 ); 
-   //end = ( ((uint64_t)cycles_high1 << 32) | cycles_low1 ); 
-
-	//printf("test = %c cycles for page hit = %llu \n",test, (end-start));	
 	
-	
-	
+	unm = munmap(op,sb.st_size);
+
+	if(unm == -1)
+		printf("UNMAP FAILED\n");
 	
 	
 	printf("Reached till here safely now exiting\n",test);
