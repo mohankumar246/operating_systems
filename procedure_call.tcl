b741a2d2854f70de2ee052a5fdd5570047747af6
Mohan Kumar N <mohankumar246@gmail.com>
2016-10-03 02:12:15 -0400

    only my files remain

--- procedure_call.tcl
+++
@@ -1,8 +0,0 @@
-./obj/procedure_call0 > results/procedure_call0.results
-./obj/procedure_call1 > results/procedure_call1.results
-./obj/procedure_call2 > results/procedure_call2.results
-./obj/procedure_call3 > results/procedure_call3.results
-./obj/procedure_call4 > results/procedure_call4.results
-./obj/procedure_call5 > results/procedure_call5.results
-./obj/procedure_call6 > results/procedure_call6.results
-./obj/procedure_call7 > results/procedure_call7.results
